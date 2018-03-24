5479c21ff41ee1fb52884210b84ddd3164a94c86
Szali Szali <szalicil@gmail.com>
2018-03-24 11:09:22 +0100

    karakterizációs tesztek

---
+++ C#/Trivia/CharTests/CharacterizationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Trivia;
+
+namespace CharTests
+{
+    [TestFixture]
+    public class CharacterizationTests
+    {
+        [Test]
+        [TestCaseSource("Seeds")]
+        public void Test(int seed)
+        {
+            string sutOutput;
+            using (var stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                GameRunner.Main(new[] { seed.ToString() });
+                stringWriter.Flush();
+                sutOutput = stringWriter.ToString();
+            }
+
+            var referenceOutput = File.ReadAllText(Path.Combine(ReferenceOutputDirectory(), seed.ToString()));
+
+            Assert.That(sutOutput, Is.EqualTo(referenceOutput));
+        }
+
+        private static IEnumerable<int> Seeds()
+        {
+            return Directory.GetFiles(ReferenceOutputDirectory())
+                .Select(Path.GetFileName)
+                .Select(int.Parse);
+        }
+
+        private static string ReferenceOutputDirectory()
+        {
+            return Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "..", "refout");
+        }
+    }
+}
